7f0fce31a5981c026d87b89579536146efa7ee63
Rixsitillo <rixsitilloxomidov@gmail.com>
2020-12-17 19:43:24 +0500

    first commit

---
+++ src/Questions.js
@@ -0,0 +1,133 @@
+export const questions = [{
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "O’zbek tili tarixini davrlashtirish qachondan boshlangan?",
+        "correct_answer": "1912 yil",
+        "answers": [
+            "1912 yil",
+            "1944 yil",
+            "1940 yil",
+            "1945 yil"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Sadr atamasi qaysi asrdan boshlab, turk, chig’atoy, o’zbek atamalari bilan parallel bo’lgan.",
+        "correct_answer": "15 asrdan 19 asr boshigacha",
+        "answers": [
+            "11 asrdan 20 asr boshigacha",
+            "15 asrdan 19 asr boshigacha",
+            "11 asrdan 15 asr boshigacha",
+            "11 asrdan 17 asr boshigacha"
+
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Tobe bog’lanishli birikmalarda qanday so’z bosh so’z sanaladi?",
+        "correct_answer": "Ma’nosi izohlanayotgan soz",
+        "answers": [
+            "Ma’nosi izohlanayotgan soz",
+            "O’zi bog’langan so’z ma’nosini izohlaydigan so’z",
+            "O’zi bog’langan so’z ma’nosini izohlaydigan so’z",
+            "Ko'makchi bilan kelgan so'z"
+
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "O'zbekistonda ishga tushishi kutilayotgan Sport avtomobillari ishlab chiqaruvchi kompaniya?",
+        "correct_answer": "Volkswagen",
+        "answers": [
+            "Toyota",
+            "Audi",
+            "Volkswagen",
+            "Ferrari"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Bitishuv munosabatidagi birikmalar berilgan qatorni aniqlang",
+        "correct_answer": "Qulliq qildi , so’z sehri",
+        "answers": [
+            "Ko’klam quyoshi , bahor tongi",
+            "Olis yo’llar, tez yurdi",
+            "Olkamiz chiroyi , ko’l suvi",
+            "Qulliq qildi , so’z sehri"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Qaysi qatordagi gapda uyushiq bo’laklar mavjud emas?",
+        "correct_answer": "Til bilan dil bir bo’lishi kerak",
+        "answers": [
+            "Salimaning baland , ammo mayin ovozi bor edi",
+            " Xalqqa qaradi-yu , ammo gapirmadi",
+            "Til bilan dil bir bo’lishi kerak",
+            "Bugun Anvar yoki O’tkir navbatchilik qiladi"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Bog’imizda qizlar , o’g’illar , Qo’shiq aytsin , yayrasin , kulsin. Ushbu gapda qaysi bo’laklar uyushib kelgan?",
+        "correct_answer": "Ega va kesim",
+        "answers": [
+            "Faqat ega",
+            "Faqat kesim",
+            "Ega va kesim",
+            "Hollar"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Ajratilgan bo’laklar yozuvda qanday tinish belgisi bilan ajratiladi?",
+        "correct_answer": "Vergul bilan",
+        "answers": [
+            "Vergul bilan",
+            "Tire bilan",
+            "Qo’shtirnoq ichiga olinadi",
+            "Vergul va tire bilan"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Undalmali gaplarning kesimi qaysi shaxslarda bo’ladi?",
+        "correct_answer": "2- va 3-shaxsda",
+        "answers": [
+            "1- va 2-shaxsda",
+            "1- va 3-shaxsda",
+            "Faqat 2-shaxsda",
+            "2- va 3-shaxsda"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Dostlar ,biz bu ishni , so’zsiz, bajaramiz. Ushbu gapdan kirish so’zni aniqlang",
+        "correct_answer": "Dostlar",
+        "answers": [
+            "Dostlar",
+            "Biz",
+            "So’zsiz",
+            "bajaramiz"
+        ]
+    }
+]
